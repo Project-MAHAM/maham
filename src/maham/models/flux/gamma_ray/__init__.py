0f28428c5aea5cff55e26b64f0634e0f85dafd95
Mohammad-Neutrino <fulhossain@ku.edu>
2026-09-15 13:10:48 -0500

    Add blazar gamma-ray population models

---
+++ src/maham/models/flux/gamma_ray/__init__.py
@@ -0,0 +1,3 @@
+from maham.models.flux.gamma_ray.source_environment import AjelloBlazarPopulation2015, KM3NeTBlazarPopulation2026GammaBestFit
+
+__all__ = ["AjelloBlazarPopulation2015", "KM3NeTBlazarPopulation2026GammaBestFit"]
